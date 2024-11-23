b99dd6bd07af7ecc1815f3c1b2c0ae8b7e880817
Brian Faust <hello@basecode.sh>
2024-11-23 17:29:43 +0200

    first commit
    Signed-off-by: Brian Faust <hello@basecode.sh>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) BaseCode Oy - All Rights Reserved
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace BaseCodeOy\BladeIcons\SimpleIcons;
+
+use BaseCodeOy\BladeIcons\Facades\IconFamilyRegistry;
+use BaseCodeOy\BladeIcons\IconFamily;
+use BaseCodeOy\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    public function packageRegistered(): void
+    {
+        IconFamilyRegistry::push(IconFamily::fromDirectory('simple-icons', __DIR__.'/../resources/svg'));
+    }
+}
